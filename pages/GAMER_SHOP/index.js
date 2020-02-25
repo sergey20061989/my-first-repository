00495ec2950e81ce1ed05a1c681ab2022bbdcee7
sergey20061989 <guns20061989@gmail.com>
2020-02-25 21:25:21 +0200

    migrate too linuks

---
+++ pages/GAMER_SHOP/index.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+    let x = 1;
+    let str = "hellow";
+    let bool = false;
+    let un = undefined;
+    let y = null
+    if (x == 1) {
+        alert("Serega " + str);
+
+    } else {
+        alert("good bey")
+
+
+    }
+}
